21d9f9a2e77c2c04e96fa23b82f314e5a5831135
N1colaOu <n.ouzounov06@gmail.com>
2026-07-27 18:48:46 +0200

    add: interface integrators with empty der classe

--- include/integrators.h
+++ include/integrators.h
@@ -2,24 +2,38 @@
 #include"body.h"
 #include<vector>
 
-class Integrator
+class Integrator//interface
 {
 public:
-    virtual void step(std::vector<Body>&, double) = 0;
+    virtual void step(std::vector<Body>&, double) const = 0;
     virtual ~Integrator() = default;
 };
 
+
 class EulerIntegrator : public Integrator
 {
-
+public:
+    void step(std::vector<Body>&, double) const override final;
 };
 
-class RK4Integrator : public EulerIntegrator
+void EulerIntegrator::step(std::vector<Body>& system, double dt) const{
+
+}
+
+class RK4Integrator : public Integrator
 {
-
+public:
+    void step(std::vector<Body>&, double) const override final;
 };
+void RK4Integrator::step(std::vector<Body>& system, double dt) const{
+
+}
 
 class VerletIntegrator : public Integrator
 {
-
+public:
+    void step(std::vector<Body>&, double) const override final;
 };
+void VerletIntegrator::step(std::vector<Body>& system, double dt) const{
+
+}
