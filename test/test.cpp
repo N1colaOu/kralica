a1e047d719db4e0dc4aabaf589561e5a9955da07
N1colaOu <n.ouzounov06@gmail.com>
2026-07-29 20:25:11 +0200

    add: test folder and test scripts

---
+++ test/test.cpp
@@ -0,0 +1,16 @@
+#include<print>
+#include"../include/body.h"
+#include"../include/integrators.h"
+#include<memory>
+
+int main(){
+    Body p1{{0.0,0.0,0.0}, {0.0,0.0,0.0}, {0.0,1.0,0.0}, 1.0};
+    std::vector<Body> system{p1};
+    std::unique_ptr<Integrator> euler(new EulerIntegrator);
+    for (size_t i = 0; i < 10; i++)
+    {
+        euler->step(system, 0.1);
+        std::print("Position {}, {}, {}\n", system[0].get_pos().get_x(), system[0].get_pos().get_y(), system[0].get_pos().get_z());
+    }
+    return 0;
+}
