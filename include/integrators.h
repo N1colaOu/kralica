6548d8afc8b082807788c593c80a122b96c15e07
N1colaOu <n.ouzounov06@gmail.com>
2026-07-30 21:44:35 +0200

    ref: separated the acc from the body and did the changes

--- include/integrators.h
+++ include/integrators.h
@@ -1,11 +1,12 @@
 #pragma once
 #include"body.h"
 #include<vector>
+#include<stdexcept>
 
 class Integrator//interface
 {
 public:
-    virtual void step(std::vector<Body>&, double) const = 0;
+    virtual void step(std::vector<Body>&, std::vector<Vector3d>&, double) const = 0;
     virtual ~Integrator() = default;
 };
 
@@ -13,25 +14,31 @@ public:
 class EulerIntegrator : public Integrator
 {
 public:
-    void step(std::vector<Body>&, double) const override final;
+    void step(std::vector<Body>&, std::vector<Vector3d>&, double) const override final;
 };
 
-void EulerIntegrator::step(std::vector<Body>& system, double dt) const{
-    for(auto& particle : system){
+void EulerIntegrator::step(std::vector<Body>& system, std::vector<Vector3d>& accs, double dt) const{
+    if(system.size() != accs.size()) throw std::invalid_argument("mismatch size of particles and accelerations");
+    size_t n = system.size();
+    //update acc here with ff
+    for (size_t i = 0; i < n; i++)
+    {  
+        Body& particle {system.at(i)};
+        Vector3d& acc {accs.at(i)};
         const double m = particle.get_mass();
-        //update force/acc here if not constant
-        //const Vector3d acc = particle.get_acc()/m;
-        particle.set_vel(particle.get_vel() + particle.get_acc()*dt);
+        particle.set_vel(particle.get_vel() + acc*dt);
         particle.set_pos(particle.get_pos() + particle.get_vel()*dt);
     }
+    
 }
 
 class RK4Integrator : public Integrator
 {
 public:
-    void step(std::vector<Body>&, double) const override final;
+    void step(std::vector<Body>&, std::vector<Vector3d>&, double) const override final;
 };
-void RK4Integrator::step(std::vector<Body>& system, double dt) const{
+void RK4Integrator::step(std::vector<Body>& system, std::vector<Vector3d>& accs, double dt) const{
+    if(system.size() != accs.size()) throw std::invalid_argument("mismatch size of particles and accelerations");
     // for(auto& particle : system){
     //     const double m = particle.get_mass();
     //     particle.set_vel(particle.get_vel() + particle.get_acc()/m*dt);
@@ -42,15 +49,21 @@ void RK4Integrator::step(std::vector<Body>& system, double dt) const{
 class VerletIntegrator : public Integrator
 {
 public:
-    void step(std::vector<Body>&, double) const override final;
+    void step(std::vector<Body>&, std::vector<Vector3d>&, double) const override final;
 };
-void VerletIntegrator::step(std::vector<Body>& system, double dt) const{
-    for(auto& particle : system){
+void VerletIntegrator::step(std::vector<Body>& system, std::vector<Vector3d>& accs, double dt) const{
+    if(system.size() != accs.size()) throw std::invalid_argument("mismatch size of particles and accelerations");
+    size_t n = system.size();
+    const std::vector<Vector3d> accs_old{accs};
+    //update accs with ff
+    for (size_t i = 0; i < n; i++)
+    {
+        Body& particle {system.at(i)};
+        Vector3d& acc {accs.at(i)};
+        const Vector3d& acc_old {accs_old.at(i)};
         const double m = particle.get_mass();
-        const Vector3d acc_old = particle.get_acc();
+        
         particle.set_pos(particle.get_pos() + particle.get_vel()*dt + acc_old*(dt*dt*0.5));
-        //update acc here 
-        //const Vector3d new_acc = particle.get_acc()/m;
-        particle.set_vel(particle.get_vel() + (particle.get_acc()+acc_old)*(dt*0.5));
+        particle.set_vel(particle.get_vel() + (acc+acc_old)*(dt*0.5));
     }
 }
--- test/test.cpp
+++ test/test.cpp
@@ -4,12 +4,14 @@
 #include<memory>
 
 int main(){
-    Body p1{{0.0,0.0,0.0}, {0.0,0.0,0.0}, {0.0,1.0,0.0}, 1.0};
+    Body p1{{0.0,0.0,0.0}, {0.0,0.0,0.0}, 1.0};
+    Vector3d acc1({0.0,1.0,0.0});
     std::vector<Body> system{p1};
+    std::vector<Vector3d> accs{acc1};
     std::unique_ptr<Integrator> euler(new EulerIntegrator);
     for (size_t i = 0; i < 10; i++)
     {
-        euler->step(system, 0.1);
+        euler->step(system, accs, 0.1);
         std::print("Position {}, {}, {}\n", system[0].get_pos().get_x(), system[0].get_pos().get_y(), system[0].get_pos().get_z());
     }
     return 0;
