2a559db01ca2dac4719cd6e56228bd51dcd58afc
N1colaOu <n.ouzounov06@gmail.com>
2026-08-11 20:58:46 +0200

    add: other run sim files for my windows pc

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-g++ -std=c++23 -O0 -I ./include -I ./glad -o ./sim test/test_lj.cpp src/visualization.cpp glad/glad.c -lglfw -ldl -lpthread
-./sim
+g++ -std=c++23 -O0 -I ./include -I ./glad -o ./sim.o test/test_lj.cpp src/visualization.cpp glad/glad.c -lglfw -ldl -lpthread
+./sim.o
