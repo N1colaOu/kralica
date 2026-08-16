88d95732299c269b018965e91ecce9486135513f
N1colaOu <n.ouzounov06@gmail.com>
2026-08-16 14:35:06 +0200

    fix:working with new vector

--- include/forcefields.h
+++ include/forcefields.h
@@ -5,41 +5,44 @@
 #include"body.h"
 #include<algorithm>
 
+using kralica::Vector2d;
+
 class ForceField
 {
 public:
-virtual void compute(const std::vector<Body>&, std::vector<Vector3d>&) const = 0;
+virtual void compute(const std::vector<Body>&, std::vector<Vector2d>&) const = 0;
 virtual ~ForceField() = default;
 };
 
 class GravityForce : public ForceField
 {
 public:
-void compute(const std::vector<Body>&, std::vector<Vector3d>&) const override;
+void compute(const std::vector<Body>&, std::vector<Vector2d>&) const override;
 };
 
-void GravityForce::compute(const std::vector<Body>& system, std::vector<Vector3d>& accs) const{
+void GravityForce::compute(const std::vector<Body>& system, std::vector<Vector2d>& accs) const{
     if(system.size() != accs.size()) throw std::invalid_argument("mismatch size of particles and accelerations");
     size_t n = system.size();
-    std::fill(accs.begin(), accs.end(), Vector3d{0.00,0.00,0.00});
+    std::fill(accs.begin(), accs.end(), Vector2d{{0.00,0.00}});
     for (size_t i = 0; i < n; i++)
     {   
         const Body& particle_i = system[i];
         const double m_i = particle_i.get_mass();
-        const Vector3d pos_i = particle_i.get_pos();
+        const Vector2d pos_i = particle_i.get_pos();
 
         for (size_t j = i + 1; j < n; j++)
         {
             const Body& particle_j = system[j];
             const double m_j = particle_j.get_mass();
-            const Vector3d pos_j = particle_j.get_pos();
-            const Vector3d rad_ij = (pos_j-pos_i);
-            const double rad_soft = 1/std::sqrt(rad_ij.len_sqrd() + R_SOFT_GRAV*R_SOFT_GRAV);
-            const double coeff = G*rad_soft*rad_soft*rad_soft;
-            const Vector3d grav_force_ij{rad_ij*coeff};
+            const Vector2d pos_j = particle_j.get_pos();
+            const Vector2d rad_ij = (pos_j-pos_i);
+            const double rad_norm = rad_ij.norm();
+            const double rad_soft = 1/std::sqrt(rad_norm*rad_norm + R_SOFT_GRAV*R_SOFT_GRAV);
+            const double force_mag = G*rad_soft*rad_soft*rad_soft;
+            const Vector2d grav_force_ij = rad_ij*force_mag;
 
-            accs[j] = accs[j] - grav_force_ij*m_i;
-            accs[i] = accs[i] + grav_force_ij*m_j;
+            accs[j] -= grav_force_ij*m_i;
+            accs[i] += grav_force_ij*m_j;
         }
     }
 }
@@ -47,24 +50,24 @@ void GravityForce::compute(const std::vector<Body>& system, std::vector<Vector3d
 class LennardJonesForce : public ForceField
 {
 public:
-void compute(const std::vector<Body>&, std::vector<Vector3d>&) const override;
+void compute(const std::vector<Body>&, std::vector<Vector2d>&) const override;
 };
 
-void LennardJonesForce::compute(const std::vector<Body>& system, std::vector<Vector3d>& accs) const{
+void LennardJonesForce::compute(const std::vector<Body>& system, std::vector<Vector2d>& accs) const{
     if(system.size() != accs.size()) throw std::invalid_argument("mismatch size of particles and accelerations");
     size_t n = system.size();
-    std::fill(accs.begin(), accs.end(), Vector3d{0.00,0.00,0.00});
+    std::fill(accs.begin(), accs.end(), Vector2d{{0.00,0.00}});
     for (size_t i = 0; i < n; i++)
     {   
         const Body& particle_i = system[i];
         const double m_i = particle_i.get_mass();
-        const Vector3d pos_i = particle_i.get_pos();
+        const Vector2d pos_i = particle_i.get_pos();
 
         for (size_t j = i + 1; j < n; j++)
         {
             const Body& particle_j = system[j];
-            const Vector3d pos_j = particle_j.get_pos();
-            const Vector3d rad_ij = (pos_j-pos_i);
+            const Vector2d pos_j = particle_j.get_pos();
+            const Vector2d rad_ij = (pos_j-pos_i);
             const double rad_norm = rad_ij.norm();
             if (rad_norm >= R_CUTOFF_LJ) continue;
             const double m_j = particle_j.get_mass();
@@ -72,9 +75,9 @@ void LennardJonesForce::compute(const std::vector<Body>& system, std::vector<Vec
             double x3 = x * x * x;
             double x6 = x3 * x3;
             double force_mag = 24.0 * EPSILON_LJ * x / (SIGMA_LJ * SIGMA_LJ) * (2.0 * x6 - x3);
-            const Vector3d lj_force_ij = rad_ij*force_mag;
-            accs[j] = accs[j] + lj_force_ij/m_j;
-            accs[i] = accs[i] - lj_force_ij/m_i;
+            const Vector2d lj_force_ij = rad_ij*force_mag;
+            accs[j] += lj_force_ij/m_j;
+            accs[i] -= lj_force_ij/m_i;
         }
     }
 }
--- include/integrators.h
+++ include/integrators.h
@@ -5,6 +5,8 @@
 #include<memory>
 #include<cmath>
 
+using kralica::Vector2d;
+
 using ff_ptr = std::unique_ptr<ForceField>;
 
 inline double wrap(double x, double half_len) {
@@ -17,7 +19,7 @@ inline double wrap(double x, double half_len) {
 class Integrator//interface
 {
 public:
-    virtual void step(std::vector<Body>&, std::vector<Vector3d>&, const ff_ptr& ,double) const = 0;
+    virtual void step(std::vector<Body>&, std::vector<Vector2d>&, const ff_ptr& ,double) const = 0;
     virtual ~Integrator() = default;
 };
 
@@ -25,18 +27,17 @@ public:
 class EulerIntegrator : public Integrator
 {
 public:
-    void step(std::vector<Body>&, std::vector<Vector3d>&, const ff_ptr&,  double) const override final;
+    void step(std::vector<Body>&, std::vector<Vector2d>&, const ff_ptr&,  double) const override final;
 };
 
-void EulerIntegrator::step(std::vector<Body>& system, std::vector<Vector3d>& accs, const ff_ptr& ff, double dt) const{
+void EulerIntegrator::step(std::vector<Body>& system, std::vector<Vector2d>& accs, const ff_ptr& ff, double dt) const{
     if(system.size() != accs.size()) throw std::invalid_argument("mismatch size of particles and accelerations");
     size_t n = system.size();
     ff->compute(system, accs);
     for (size_t i = 0; i < n; i++)
     {  
         Body& particle {system[i]};
-        const Vector3d& acc {accs[i]};
-        const double m = particle.get_mass();
+        const Vector2d& acc {accs[i]};
         particle.set_vel(particle.get_vel() + acc*dt);
         particle.set_pos(particle.get_pos() + particle.get_vel()*dt);
     }
@@ -46,9 +47,9 @@ void EulerIntegrator::step(std::vector<Body>& system, std::vector<Vector3d>& acc
 class RK4Integrator : public Integrator
 {
 public:
-    void step(std::vector<Body>&, std::vector<Vector3d>&, const ff_ptr&, double) const override final;
+    void step(std::vector<Body>&, std::vector<Vector2d>&, const ff_ptr&, double) const override final;
 };
-void RK4Integrator::step(std::vector<Body>& system, std::vector<Vector3d>& accs, const ff_ptr& ff, double dt) const{
+void RK4Integrator::step(std::vector<Body>& system, std::vector<Vector2d>& accs, const ff_ptr& ff, double dt) const{
     if(system.size() != accs.size()) throw std::invalid_argument("mismatch size of particles and accelerations");
     size_t n = system.size();
     //v_new = v_old + 1/6*dt*(a1+2a2+2a3+a4)
@@ -58,30 +59,28 @@ void RK4Integrator::step(std::vector<Body>& system, std::vector<Vector3d>& accs,
 class VerletIntegrator : public Integrator
 {
 public:
-    void step(std::vector<Body>&, std::vector<Vector3d>&, const ff_ptr&, double) const override final;
+    void step(std::vector<Body>&, std::vector<Vector2d>&, const ff_ptr&, double) const override final;
 };
-void VerletIntegrator::step(std::vector<Body>& system, std::vector<Vector3d>& accs, const ff_ptr& ff, double dt) const{
+void VerletIntegrator::step(std::vector<Body>& system, std::vector<Vector2d>& accs, const ff_ptr& ff, double dt) const{
     if(system.size() != accs.size()) throw std::invalid_argument("mismatch size of particles and accelerations");
     size_t n = system.size();
     for (size_t i = 0; i < n; i++)
     {
         Body& particle {system.at(i)};
-        const Vector3d& acc {accs.at(i)};
-        const double m = particle.get_mass();
+        const Vector2d& acc {accs.at(i)};
         
         particle.set_vel(particle.get_vel() + acc*(dt*0.5));
 
-        Vector3d new_pos = particle.get_pos() + particle.get_vel()*dt + acc*(dt*dt*0.5);
-        new_pos.set_x(wrap(new_pos.get_x(), BOX_SIZE));
-        new_pos.set_y(wrap(new_pos.get_y(), BOX_SIZE));
-        new_pos.set_z(wrap(new_pos.get_z(), BOX_SIZE));
+        Vector2d new_pos = particle.get_pos() + particle.get_vel()*dt + acc*(dt*dt*0.5);
+        new_pos[0] = (wrap(new_pos[0], BOX_SIZE));
+        new_pos[1] = (wrap(new_pos[1], BOX_SIZE));
         particle.set_pos(new_pos);
     }
     ff->compute(system, accs);    
     for (size_t i = 0; i < n; i++)
     {
         Body& particle {system.at(i)};
-        const Vector3d& acc {accs.at(i)};  
+        const Vector2d& acc {accs.at(i)};  
         particle.set_vel(particle.get_vel() + acc*(dt*0.5));
     }
     
