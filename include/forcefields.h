78fe47268b5de2e294b0d0bc58387f6613dd4faf
N1colaOu <n.ouzounov06@gmail.com>
2026-08-16 20:05:19 +0200

    fix: removed hard cutoff chek for small r

--- include/forcefields.h
+++ include/forcefields.h
@@ -5,7 +5,6 @@
 #include"body.h"
 #include<algorithm>
 
-using kralica::Vector2d;
 
 class ForceField
 {
@@ -23,7 +22,6 @@ void compute(const std::vector<Body>&, std::vector<Vector2d>&) const override;
 void GravityForce::compute(const std::vector<Body>& system, std::vector<Vector2d>& accs) const{
     if(system.size() != accs.size()) throw std::invalid_argument("mismatch size of particles and accelerations");
     size_t n = system.size();
-    std::fill(accs.begin(), accs.end(), Vector2d{{0.00,0.00}});
     for (size_t i = 0; i < n; i++)
     {   
         const Body& particle_i = system[i];
@@ -36,9 +34,8 @@ void GravityForce::compute(const std::vector<Body>& system, std::vector<Vector2d
             const double m_j = particle_j.get_mass();
             const Vector2d pos_j = particle_j.get_pos();
             const Vector2d rad_ij = (pos_j-pos_i);
-            const double rad_norm = rad_ij.norm();
-            if (rad_norm <= R_CRIT_GRAV) continue;
-            const double rad_soft = 1/std::sqrt(rad_norm*rad_norm + R_SOFT_GRAV*R_SOFT_GRAV);
+            const double rad_norm2 = rad_ij[0]*rad_ij[0] + rad_ij[1]*rad_ij[1];
+            const double rad_soft = 1/std::sqrt(rad_norm2 + R_SOFT_GRAV*R_SOFT_GRAV);
             const double force_mag = G*rad_soft*rad_soft*rad_soft;
             const Vector2d grav_force_ij = rad_ij*force_mag;
 
@@ -57,7 +54,6 @@ void compute(const std::vector<Body>&, std::vector<Vector2d>&) const override;
 void LennardJonesForce::compute(const std::vector<Body>& system, std::vector<Vector2d>& accs) const{
     if(system.size() != accs.size()) throw std::invalid_argument("mismatch size of particles and accelerations");
     size_t n = system.size();
-    std::fill(accs.begin(), accs.end(), Vector2d{{0.00,0.00}});
     for (size_t i = 0; i < n; i++)
     {   
         const Body& particle_i = system[i];
@@ -69,10 +65,10 @@ void LennardJonesForce::compute(const std::vector<Body>& system, std::vector<Vec
             const Body& particle_j = system[j];
             const Vector2d pos_j = particle_j.get_pos();
             const Vector2d rad_ij = (pos_j-pos_i);
-            const double rad_norm = rad_ij.norm();
-            if (rad_norm >= R_CUTOFF_LJ) continue;
+            const double rad_norm2 = rad_ij[0]*rad_ij[0] + rad_ij[1]*rad_ij[1];
+            if (rad_norm2 >= R_CUTOFF_LJ) continue;
             const double m_j = particle_j.get_mass();
-            const double x = SIGMA_LJ * SIGMA_LJ / (rad_norm * rad_norm + R_SOFT_LJ * R_SOFT_LJ);
+            const double x = SIGMA_LJ * SIGMA_LJ / (rad_norm2 + R_SOFT_LJ * R_SOFT_LJ);
             double x3 = x * x * x;
             double x6 = x3 * x3;
             double force_mag = 24.0 * EPSILON_LJ * x / (SIGMA_LJ * SIGMA_LJ) * (2.0 * x6 - x3);
