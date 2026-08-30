668c005011df545779935fcc04b9d77a4da88a85
N1colaOu <n.ouzounov06@gmail.com>
2026-08-30 22:02:20 +0200

    add: log and file output for simulation

--- include/simulation.h
+++ include/simulation.h
@@ -3,6 +3,8 @@
 #include<vector>
 #include<numeric>
 #include<print>
+#include<string>
+#include<fstream>
 #include<stdexcept>
 
 namespace kralica{
@@ -20,13 +22,15 @@ namespace kralica{
             CompositeField&, double);
         void step();
         void run(double);
+        void run_and_log(double, const std::string&);
         void print_statistics() const;
+        void log_statistics(std::ofstream&) const;
     };
     Simulation::Simulation(const std::vector<Body>& b,
             const std::vector<Vector2d>& a, std::unique_ptr<Integrator>& i, 
             CompositeField& ffs, double dt_) : 
             system(b), accs(a), integrator(std::move(i)), force_fields(ffs), dt(dt_){
-                if(!i) throw(std::invalid_argument("Invalid Integrator"));
+                if(!integrator) throw(std::invalid_argument("Invalid Integrator"));
                 if(b.size() != a.size()) throw(std::invalid_argument("Mismtach size"));
             }
     
@@ -53,5 +57,23 @@ namespace kralica{
             return v*v*x.get_mass()*0.50;
         });
         std::print("Total Energy: {}", energy);
+    }
+    void Simulation::log_statistics(std::ofstream& out) const{
+        std::size_t n = system.size();      
+        for (size_t i = 0; i < n; i++)
+        {
+            const Body& part = system[i];
+            std::println(out, "{}, {}, {}, {}", part.get_pos(), part.get_vel(), accs[i], part.get_mass());
+        }
+    }
+    void Simulation::run_and_log(double end_time, const std::string& file_name){
+        std::ofstream out(file_name, std::ios::out | std::ios::trunc);
+        std::println(out, "pos, vel, acc, m");
+        while(current_time <= end_time){
+            log_statistics(out);
+            step();
+        }
+        log_statistics(out);
+        out.close();
     }
 }
